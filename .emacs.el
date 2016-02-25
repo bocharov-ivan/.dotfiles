bc5692d79cf26e1820af2d58b400f76c66e20b17
Ivan Bocharov <bocharovia@gmail.com>
2016-02-25 22:06:54 +0100

    Added some TeX-related settings.

--- .emacs.el
+++ .emacs.el
@@ -6,6 +6,13 @@
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
+(when (member "Hack" (font-family-list))
+  (add-to-list 'initial-frame-alist' (font . "Hack-9"))
+  (add-to-list 'default-frame-alist' (font . "Hack-9"))
+)
+
+(add-to-list 'default-frame-alist' (fullscreen . maximized))
+
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize)
     (exec-path-from-shell-copy-env "GOPATH"))
@@ -24,6 +31,10 @@
 (setq neo-show-hidden-files t)
 
 (menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq ring-bell-function 'ignore)
+;(toggle-frame-fullscreen)
 
 (require 'powerline)
 (powerline-default-theme)
@@ -38,7 +49,7 @@
 (setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
-(require 'go-flymake)
+;(require 'go-flymake)
 
 (require 'auto-complete)
 (require 'auto-complete-config)
@@ -51,8 +62,27 @@
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 
+(when (eq system-type 'gnu/linux)
+  (pdf-tools-install)
+  (setq TeX-view-program-selection '((output-pdf "pdf-tools")))
+  (setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
+)
+
 (setq LaTeX-item-indent 0)
 (setq LaTeX-indent-level 2)
 
-(load-theme 'zenburn t)
+(load-theme 'base16-ocean-dark t)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
